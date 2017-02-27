2dcc6bb696bfdae4290b30a9491ac3372ba38584
Patrick Niyongabo <patrickniyongabo@gmail.com>
2017-02-26 22:52:26 -0600

    added map & filter & reduce intro -  adding content tomorrow

---
+++ mapFilterReduce.py
@@ -0,0 +1,7 @@
+# 1. MAP: map is very useful when applying a function to all elements in a list.
+
+# 2. FILTER: filter only returns elements for which the passed function is TRUE.
+
+# 3. REDUCE: from left to right, apply function to list, two elements at a time.
+# Start by setting _acc to func(lst[0], lst[1]), and then to func(_acc, lst[2])
+
