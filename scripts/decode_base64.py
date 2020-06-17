58b5dda1940a4730e8bbacbadca2e15127f2591d
pniyongabo <patrickniyongabo@gmail.com>
2020-06-16 18:06:57 -0700

    added sample files and renamed scripts

--- scripts/decode_base64.py
+++ scripts/decode_base64.py
@@ -2,7 +2,7 @@
 
 import base64
 
-f = open('./b64_file.txt', 'rb')
-decoded_str = base64.b64decode(f.read())
+f = open('../sample_files/glossary_b64.txt', 'rb')
+decoded_str = base64.b64decode(f.read()).decode('utf-8')
 print(decoded_str)
 f.close()
--- scripts/decode_base64_gzip.py
+++ scripts/decode_base64_gzip.py
@@ -3,7 +3,7 @@
 import base64
 import zlib
 
-f = open('./gzip_b64_file.txt', 'rb')
+f = open('../sample_files/glossary_b64_gzip.txt', 'rb')
 decoded_str = zlib.decompress(base64.b64decode(f.read()), 16 + zlib.MAX_WBITS).decode('utf-8')
 print(decoded_str)
 f.close()
