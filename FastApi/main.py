46b5a2062b1a8f901e002757360bf6f83403b0e0
pniyongabo <patrickniyongabo@gmail.com>
2020-07-15 19:45:56 -0700

    fastapi - added path variable enum-based validation

--- FastApi/main.py
+++ FastApi/main.py
@@ -1,6 +1,13 @@
+from enum import Enum
+
 from fastapi import FastAPI
 
 app = FastAPI()
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 
 @app.get("/")
@@ -15,3 +22,14 @@ async def root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+
+@app.get("/model/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
