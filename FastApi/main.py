75a5c56fb0209f4e2d1a626f706ddb6ede7d039b
pniyongabo <patrickniyongabo@gmail.com>
2020-07-16 13:52:48 -0700

    fastapi - Multiple path and query parameters

--- FastApi/main.py
+++ FastApi/main.py
@@ -42,6 +42,21 @@ async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
     return item
 
 
+# http://127.0.0.1:8000/users/10/items/foo?q=letterQ&short=off
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Optional[str] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
 @app.get("/model/{model_name}")
 async def get_model(model_name: ModelName):
     if model_name == ModelName.alexnet:
